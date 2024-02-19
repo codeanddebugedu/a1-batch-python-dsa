61d75063a7a63fccd2c219cabda88655f9770262
Code and Debug <codeanddebugedu@gmail.com>
2024-02-19 22:28:13 +0530

    oops

---
+++ 16. OOPS/oop.py
@@ -0,0 +1,9 @@
+student_details = {
+    4561: {"name": "Anirudh", "age": 56, "gender": "Male"},
+    124: {"name": "Akul", "age": 16, "gender": "Male"},
+    8769: {"name": "Vandana", "age": 42, "gender": "Female"},
+}
+
+student_details[8769]["name"] = "Vandy"
+
+print(student_details)
